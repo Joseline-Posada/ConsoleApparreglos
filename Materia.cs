30776ab028ffae4532c6478bd47358d2f7ea6622
josel <josel@LAPTOP-8FA433NA>
2021-11-01 14:36:51 -0600

    Agregar archivos de proyecto.

---
+++ Materia.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApparreglos
+{
+    class Materia
+    {
+        private String nombre;
+        private String profesor;
+        private int matricula;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public string Profesor
+        {
+            get
+            {
+                return profesor;
+            }
+            set
+            {
+                profesor = value;
+            }
+        }
+        public int Matricula
+        {
+            get
+            {
+                return matricula;
+            }
+            set
+            {
+                matricula = value;
+            }
+        }
+
+        public Materia ()
+        {
+          nombre= "" ;
+          profesor = "";
+          matricula = 0 ;
+    }
+    public Materia(string nombre, string profesor, int matricula)
+    {
+        this.nombre = nombre;
+        this.profesor = profesor;
+        this.matricula = matricula;
+    }
+        public override string ToString()
+        {
+            return nombre  +  " "  +  profesor  +  " "  +  matricula + " " ;
+        }
+     }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApparreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Materia[] arreglomaterias = new Materia[5];
+            for (int i = 0; i < arreglomaterias.Length; i++) ;
+            {
+                int i = 0;
+                arreglomaterias[i] = new Materia();
+                Console.WriteLine("nombre: ");
+                arreglomaterias[i].Nombre = Console.ReadLine();
+                Console.WriteLine("profesor: ");
+                arreglomaterias[i].Profesor = Console.ReadLine();
+                Console.WriteLine("matricula: ");
+                arreglomaterias[i].Matricula = Console.Read();
+            }
+            for (int i = 0; i < arreglomaterias.Length; i++) ;
+            {
+                Console.WriteLine(arreglomaterias[0].ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
